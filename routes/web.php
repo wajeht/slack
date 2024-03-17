40e76954c292de2132bd4b9060d860c463887f90
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-17 00:50:29 -0500

    feat: Add health check endpoint

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,20 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return response()->redirectTo('/login');
+});
+
+Route::get('/healthz', function (Request $request) {
+    if ($request->expectsJson()) {
+        return response()->json([
+            'message' => 'ok'
+        ]);
+    }
+
+    return response("ok", 200)->header('Content-Type', 'text/html');
 });
 
 Route::get('/dashboard', function () {
