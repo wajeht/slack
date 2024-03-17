59bb9cf4714181a714c00b46246dc31c47e8efb4
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-17 15:38:08 -0500

    test: Add tests for reaching healthz endpoint with different headers

--- tests/Feature/HealthCheckTest.php
+++ tests/Feature/HealthCheckTest.php
@@ -1,6 +1,13 @@
 <?php
 
-it('should be able to reach healthz end point', function() {
+it('should be able to reach healthz end point with json header', function() {
     $this->getJson('/healthz')
+        ->assertHeader('Content-Type', 'application/json')
         ->assertJson(['message' => 'ok']);
+});
+
+it('should be able to reach healthz end point with html header', function() {
+    $this->get('/healthz')
+        ->assertHeader('Content-Type', 'text/html; charset=UTF-8')
+        ->assertSee('ok');
 });
